222e26b45d36eed5c2e301f6d18fe65b65506d03
Alexyndr <alexyndr@gmail.com>
2018-06-16 18:18:59 +0300

    sha2

--- coding.rb
+++ coding.rb
@@ -9,19 +9,23 @@ world = STDIN.gets.chomp
 if world.is_a?(String) and world != nil
 
   puts "What method of coding do you choice?  ? \n1. MD5
-\n2. SHA1"
+\n2. SHA1 \n3. SHA2"
 
   input_num = STDIN.gets.to_i
 
   if input_num.is_a?(Numeric) and input_num != nil
 
-    if number == 1
+    if input_num == 1
 
       cod_world = Digest::MD5.hexdigest "#{world}"
 
-    elsif number == 2
+    elsif input_num == 2
 
       cod_world = Digest::SHA1.hexdigest "#{world}"
+
+    elsif input_num == 3
+
+      cod_world = Digest::SHA2.hexdigest "#{world}"
 
     end
 
